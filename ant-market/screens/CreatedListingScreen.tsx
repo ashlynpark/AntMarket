9271bc1bcd228bb240e965a9d18402fe423cf14a
plwidjaj <plwidjaj@uci.edu>
2023-05-31 22:30:35 -0700

    Fixed scroll, added profile card and all buttons

--- ant-market/screens/CreatedListingScreen.tsx
+++ ant-market/screens/CreatedListingScreen.tsx
@@ -4,47 +4,34 @@ import Header from '../components/Header';
 import { FontAwesome5 } from '@expo/vector-icons';  
 import { MaterialIcons } from '@expo/vector-icons'; 
 import React, { useState } from 'react';
-import { IconButton } from '@react-native-material/core';
+import { FAB, Icon, IconButton } from '@react-native-material/core';
 import { AntDesign } from '@expo/vector-icons'; 
 import { LinearGradient } from "expo-linear-gradient";
 import Svg, {Circle} from 'react-native-svg';
 
 
-const CreatedListingScreen = () => {
 
-    const img = 'https://images.unsplash.com/photo-1623678624314-c3efb63fb2d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1933&q=80'
-    const title = "Desk lamp"
-    const price = "$10.00"
+
+const CreatedListingScreen = () => {
 
     return(
         <Screen preset="scroll">
             <View>
                 <Header/>
                 <SafeAreaView>
-                    <View style={styles.imgContainer}>
-                        <Image source={{uri: img}}
-                            style = {{height: 230, width: 375, flex: 1, resizeMode: 'cover'}} />
-                    </View>
-                    <View style={styles.listingLabel}>
-                        <View style={styles.leftLabel}> 
-                            <Text style={styles.listingTitle}>
-                                {title}
-                            </Text>
-                        </View>
-                        <View style={styles.rightLabel}>
-                            <Text style={styles.listingTitle}>
-                                {price}
-                            </Text>
-                        </View>
-                    </View>
-                    <View style={styles.descriptionContainer}>
-                        <Text>
-                            <Text style={{fontWeight: 'bold'}}>Description: </Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dui leo, posuere ut luctus sed, tincidunt at purus. Integer eros nunc, aliquam ut posuere sed, tempor id neque.
-                        </Text>
-                    </View>
+                <View>
+                    <IMG></IMG>
+                    <ListingLabel/>
+                    <ProfileCard/>
+                    <Description/>
                     <View style={styles.allTags}>
                         <Tags tags={["office", "lamp", "desk"]}/>
                     </View>
+                    <View style={styles.saveAddContainer}>
+                        <AddtoCartButton/>
+                        <SaveButton/>
+                    </View>
+                </View>
                 </SafeAreaView>                
             </View>
         </Screen>
@@ -60,22 +47,6 @@ const CreatedListingScreen = () => {
 
 
 
-const Listing = (props: {listing : any}) => {
-    return(
-        <TouchableOpacity style={styles.listingCard}>
-            <View style={{flex: 1.2}}>
-                <Image source={{uri: props.listing.img}} style={styles.listingImg} />
-            </View>
-            <View style={{flex: 1, justifyContent: 'space-between', padding: '3%'}}>
-                <Text style={styles.listingTitle} numberOfLines={3} ellipsizeMode="tail">{props.listing.title}</Text>
-                <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-                    <Text style={styles.listingPrice}>${props.listing.price}</Text>
-                    <Text style={[styles.listingPrice, {color: 'black'}]}>View {'>'}</Text>
-                </View>
-            </View>
-        </TouchableOpacity>
-    )
-}
 
 const Tags = (props: {tags:string[]}) =>{
     return(
@@ -92,12 +63,109 @@ const Tags = (props: {tags:string[]}) =>{
     )
 }
 
+const IMG = () => {
+
+    const img = 'https://images.unsplash.com/photo-1623678624314-c3efb63fb2d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1933&q=80'
+
+    return (
+        <View style={styles.imgContainer}> 
+            <Image source={{uri: img}}
+                style = {{height: 230, width: 375, flex: 1, resizeMode: 'cover'}} />
+        </View>
+    )
+}
+
+const ListingLabel = () => {
+
+    const title = "Desk lamp"
+    const price = "$10.00"
+
+    return (
+
+        <View style={styles.listingLabel}>
+                        <View style={styles.leftLabel}> 
+                            <Text style={styles.listingTitle}>
+                                {title}
+                            </Text>
+                        </View>
+                        <View style={styles.rightLabel}>
+                            <Text style={styles.listingTitle}>
+                                {price}
+                            </Text>
+                        </View>
+        </View>
+
+    )
+
+}
+
+const Description = () => {
+    return( 
+        <View style={styles.descriptionContainer}>
+             <Text style={{fontSize: 16}}>
+                <Text style={{fontWeight: 'bold'}}>Description: </Text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dui leo, posuere ut luctus sed, tincidunt at purus. Integer eros nunc, aliquam ut posuere sed, tempor id neque.
+            </Text>
+        </View>
+    )
+
+
+}
+
+const SaveButton = () => {
+    return(
+        <View style={styles.saveButtonPosition}>
+            <TouchableOpacity style={styles.saveButton}>
+                <MaterialIcons name='bookmark-border' size={37}></MaterialIcons>
+            </TouchableOpacity>
+        </View>
+    )
+
+}
+
+const AddtoCartButton = () => {
+    return(
+        <View style={styles.addButtonPosition}>
+            <TouchableOpacity style={styles.addButton}>
+                <Text style={styles.addButtonTitle}> Add to Cart </Text>
+            </TouchableOpacity>
+        </View>
+    )
+
+}
+
+const ProfileCard = () => {
+    return(
+        <View style={styles.profileContainer}>
+            <Image style={{height: 45, width: 45}} source={require("../assets/user.png")}></Image>
+            <View style={styles.usernameAndRating}>
+              <Text style={styles.username}> petr_anteatr </Text>
+              <Image style={{height: 20, width: 100}} source={require("../assets/rating.png")}/>
+            </View>
+            <View style={{justifyContent: 'flex-end', flex: 1}}>            
+              <MessageButton/>  
+            </View>
+        </View>
+    )
+
+}
+
+const MessageButton = () => {
+  return(
+      <View>
+          <TouchableOpacity style={styles.messageButton}>
+              <Text style={{fontSize: 15, color: "black"}}> MESSAGE </Text>
+          </TouchableOpacity>
+      </View>
+  )
+
+}
+
 const styles = StyleSheet.create({
     imgContainer: {
         width: "100%",
-        height: 230,
+        marginTop: '-5%',
         position: 'absolute',
-        top: -20,
         resizeMode: 'cover',
     
     },
@@ -107,7 +175,8 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: 'row',
         height: 50,
-        top: 210,
+        marginTop: '56%',
+
     },
 
     leftLabel: {
@@ -129,12 +198,41 @@ const styles = StyleSheet.create({
 
     },
 
+    profileContainer: {
+      flex: 1,
+      flexDirection: 'row',
+      marginTop: '3%',
+      paddingLeft: '7%'
+    },
+
+    username: {
+      paddingTop: '2%',
+      fontSize: 16,
+    },
+
+    usernameAndRating: {
+      marginLeft: '4%',
+      justifyContent:'flex-start',
+      flex: 1,
+
+    },
+
+    messageButton: {
+      width: 110,
+      height: 40,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: "#fff",
+      borderWidth: 1,
+      borderColor: '#A6C48A',
+    },
+
     descriptionContainer: {
-        top: 240,
         justifyContent: 'center',
         alignItems: 'center',
         paddingLeft:'7%',
         paddingRight:'7%',
+        marginTop: '5%',
 
 
     },
@@ -147,14 +245,74 @@ const styles = StyleSheet.create({
     },
 
     allTags: {
-        top: 260,
         flexDirection: 'row',
-        paddingLeft: '7%'
+        paddingLeft: '6%',
+        marginTop: '5%',
+
+    },
+
+    saveButtonPosition: {
+        justifyContent:'flex-end',
+        flex: 1,
+        paddingRight: '9%',
+    },
+
+    saveButton: {
+        width: 53,
+        height: 53,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 60,
+        backgroundColor: "white",
+        borderWidth: 1,
+        borderColor: '#A6C48A',
+    },
+
+    addButtonPosition: {
+        justifyContent:'flex-start',
+        paddingLeft: '5%',
+        flex: 1,
+
+    },
+
+    addButton: {
+        width: 150,
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 10,
+        backgroundColor: "#678D58",
+        borderWidth: 1,
+        borderColor: '#A6C48A',
+        
+
+    },
+
+    addButtonTitle: {
+        fontSize: 17,
+        color: 'white',
+        justifyContent: 'center',
+
+    },
+
+    saveAddContainer: {
+        flexDirection: 'row',
+        flex: 1,
+        width: 600,
+        marginTop: '4%',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+
+    testContainer: {
+        paddingTop: 350,
     },
 
     defaultFont: {
         fontFamily: 'OpenSans_300Light'
     },
+
     buttonContainer: {
         padding: '2%',
         flex: 1,
@@ -178,75 +336,8 @@ const styles = StyleSheet.create({
         elevation: 5,
         marginHorizontal: '2%'
     },
-    header: {
-        fontSize: 22,
-        fontFamily: 'OpenSans_700Bold',
-        color: '#113125'
-    },
-    
-    postButtLabel: {
-        fontSize: 20,
-        fontFamily: 'OpenSans_600SemiBold',
-        alignSelf: 'center',
-        textAlign: 'center',
-        color: 'white'
-    },
-    postButtImg: {
-        borderRadius: 10,
-        height: '100%',
-        width: '100%',
-        overflow: 'hidden',
-        resizeMode: 'cover'
-    },
-    listingCard: {
-        borderRadius: 10,
-        alignSelf: 'center',
-        flexDirection: 'row',
-        flex: 1,
-        width: '100%',
-        marginVertical: '4%',
-        height: 120,
-        backgroundColor: 'white',
-        shadowColor: 'black',
-        shadowOffset: {width: 0, height: 0},
-        shadowOpacity: 0.25,
-    },
-    listingImg: {
-        height: '100%',
-        borderBottomLeftRadius: 10,
-        borderTopLeftRadius: 10,
-        overflow: 'hidden',
-        resizeMode: 'cover'
 
-    },
-    listingPrice: {
-        fontSize: 14,
-        fontFamily: 'OpenSans_500Medium',
-        marginVertical: '5%',
-        color: '#678D58'
-    },
-    newPostImgContainer: {
-        borderRadius: 10,
-        alignSelf: 'center',
-        flexDirection: 'row',
-        flex: 1,
-        width: '90%',
-        marginVertical: '5%',
-        height: 190,
-        backgroundColor: 'black',
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderWidth: 2,
-        borderColor: '#678D58' 
-    },
-    newPostImg: {
-        borderRadius: 10,
-        width: '100%',
-        overflow: 'hidden',
-        resizeMode: 'contain',
-        height: '100%',
-        backgroundColor: 'rgba(0, 0, 0, 0,8)'
-    },
+
 })
 
 export default CreatedListingScreen;
